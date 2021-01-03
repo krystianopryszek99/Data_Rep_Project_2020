d01d604274fa772c839bacb24a75ff968b6e9c05
Krystian Opryszek <57759154+krystianopryszek99@users.noreply.github.com>
2021-01-03 14:44:27 +0000

    code corrections

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 4000
+//include Cors
 const cors = require('cors');
 //include body parser
 const bodyParser = require("body-parser");
+//include mongo
 const mongoose = require('mongoose');
+
+//using cors package everytime 
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,17 +24,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//local mongo db
+//replace the admin:<password> to your admin name 
 const myConnectionString = 'mongodb+srv://krystianopryszek:Kozaktoja19@cluster0.sikdk.mongodb.net/orders?retryWrites=true&w=majority';
 mongoose.connect(myConnectionString, { useNewUrlParser: true });
 
+//define Schema 
 const Schema = mongoose.Schema;
 //define schema
 var orderSchema = new Schema({
-    // title: String,
-    // year: String,
-    // poster: String
-
-    //orderId: String,
+   
     departureCountry: String,
     destinationCountry: String,
 
@@ -48,22 +59,11 @@ var orderSchema = new Schema({
 
 });
 
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 //enteract with database, refer to OrderModel that will write to the database
 var OrderModel = mongoose.model("order", orderSchema);
 
 app.get('/api/orders/:id', (req, res) =>{
     console.log(req.params.id);
-
-    //* THIS COULD ALSO BE USED FOR SEARCH BAR * 
 
     //use the id and find it in the database
     OrderModel.findById(req.params.id, (err, data) =>{
@@ -82,109 +82,76 @@ app.put('/api/orders/:id', (req, res) => {
             res.send(data);
         })
 })
-  
+ 
+//listening for http delete method 
 app.delete('/api/orders/:id', (req, res) => {
+    //trapping the actual ID so we know that we can get the ID 
     console.log("Delete order: " + req.params.id);
-
+    //match the id back here 
     OrderModel.findByIdAndDelete(req.params.id, (err, data) => {
         res.send(data);
     })
 })
 
+//New Root point 
 app.get('/api/orders', (req, res) => {
-
-    // const mymovies = [
-    //     {
-    //         "Title":"Avengers: Infinity War",
-    //         "Year":"2018",
-    //         "imdbID":"tt4154756",
-    //         "Type":"movie",
-    //         "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    //         },
-    //         {
-    //         "Title":"Captain America: Civil War",
-    //         "Year":"2016",
-    //         "imdbID":"tt3498820",
-    //         "Type":"movie",
-    //         "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    //         }
-    // ];
 
     //interact with the database
     OrderModel.find((err, data) => {
         res.json(data);
     })
 
-    // res.status(200).json({
-    //     message: "Everything is ok",
-    //     movies: mymovies
-    // });
 })
 
+//listening to post request
 app.post('/api/orders', (req, res) => {
-    /*console.log('Movie Received!');
-    console.log(req.body.title);
-    console.log(req.body.year);
-    console.log(req.body.poster);*/
-
-    //console.log(req.body.orderId);
-
+    //when data is passed up, it will take the body of the things passed up and include it 
     console.log(req.body.departureCountry);
     console.log(req.body.destinationCountry);
 
     console.log(req.body.senderForename);
-    console.log(req.body.receiverForename);
-
     console.log(req.body.senderSurname);
-    console.log(req.body.receiverSurname);
-
     console.log(req.body.senderEmail);
-    console.log(req.body.receiverEmail);
-
     console.log(req.body.senderContact);
-    console.log(req.body.receiverContact);
-
     console.log(req.body.senderAddress);
+
+    console.log(req.body.receiverForename);
+    console.log(req.body.receiverSurname);
+    console.log(req.body.receiverEmail);
+    console.log(req.body.receiverContact);
     console.log(req.body.receiverAddress);
 
     console.log(req.body.contents);
     console.log(req.body.value);
     console.log(req.body.weight);
     console.log(req.body.totalSize);
-
-
-
+    
+    //Interacting with the model
     OrderModel.create({
-        //title: req.body.title,
-        //year: req.body.year,
-        //poster: req.body.poster
-
-        //orderId: req.body.orderId,
-
+    
         departureCountry: req.body.departureCountry,
         destinationCountry: req.body.destinationCountry,
 
         senderForename: req.body.senderForename,
-        receiverForename: req.body.receiverForename,
-
         senderSurname: req.body.senderSurname,
-        receiverSurname: req.body.receiverSurname,
-
         senderEmail: req.body.senderEmail,
-        receiverEmail: req.body.receiverEmail,
-
         senderContact: req.body.senderContact,
-        receiverContact: req.body.receiverContact,
-
         senderAddress: req.body.senderAddress,
+
+        receiverForename: req.body.receiverForename,
+        receiverSurname: req.body.receiverSurname,
+        receiverEmail: req.body.receiverEmail,
+        receiverContact: req.body.receiverContact,
         receiverAddress: req.body.receiverAddress,
 
         contents: req.body.contents,
         value: req.body.value,
         weight: req.body.weight,
         totalSize: req.body.totalSize
+        
     })
 
+    //so it doen't add a duplicate
     res.send('Item Added');
 })
 
